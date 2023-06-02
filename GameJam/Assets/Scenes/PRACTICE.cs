2c4a53481e986425239e23d7b2c42e7bca8cf6ef
Alissen Moreno <alissen.moreno01@utrgv.edu>
2023-06-02 13:59:02 -0500

    Added Assets

---
+++ GameJam/Assets/Scenes/PRACTICE.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// SO FOR SOME REASON IT DOESN'T WANNA UPLOAD SO I'M GOING OLD STYLE 
+public class PRACTICE : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
