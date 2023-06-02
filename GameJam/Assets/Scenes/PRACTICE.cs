88cf0e497067d2d2bea7da1aa4cba5ebc16273c1
Alissen Moreno <alissen.moreno01@utrgv.edu>
2023-06-02 15:50:53 -0500

    Test of gitignore

--- GameJam/Assets/Scenes/PRACTICE.cs
+++ GameJam/Assets/Scenes/PRACTICE.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 // SO FOR SOME REASON IT DOESN'T WANNA UPLOAD SO I'M GOING OLD STYLE 
+
+/// another update to see if the library will commit to github
 public class PRACTICE : MonoBehaviour
 {
     // Start is called before the first frame update
